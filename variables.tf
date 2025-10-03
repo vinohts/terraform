9f969f9908e470f5c842222e619405b650d0d4f3
vinohts <138511813+vinohts@users.noreply.github.com>
2025-10-03 12:12:28 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "aws_region" {
+  description = "AWS region"
+  default     = "ap-south-1"
+}
+
+variable "bucket_prefix" {
+  description = "S3 bucket name prefix"
+  default     = "my-jenkins-bucket"
+}
